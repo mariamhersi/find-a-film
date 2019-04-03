4673f218fb53e1420d7e0570beece364baf5a4ef
Mariam <mariamhersi@live.com>
2019-04-03 19:52:07 +0100

    First Import

---
+++ movie_database_app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import requests as apirequest
+import json
+
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    movies = []
+    if request.args.get("search") is not None:
+        search = request.args.get("search")
+    #else:
+        # search = "star wars"
+        #return render_template("error.html")
+        response = apirequest.get("http://www.omdbapi.com/?apikey=39907301&s={search}")
+        data = response.json()
+        movies = data["Search"]
+        return render_template("index.html", movies=movies)
+#else:
+        #return "hi"
+
+#@app.route("/<movie_name>")
+#def display_movie_data(movie_name):
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
