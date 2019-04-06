3b83bde7506f872fbf101bdfdc23c8264d507bc8
hafizaali <43960202+hafizaali@users.noreply.github.com>
2019-04-06 17:57:35 +0100

    Random movies generator
    Added a new page with a random movies generator button, and linked this page in the home page.

---
+++ templates/javascript.js
@@ -0,0 +1,22 @@
+var quotes = [
+  'The Dark Knight',
+  'Inception',
+  'The Departed',
+  'Heat',
+  'Leon: The Professional',
+  'Black Swan',
+  'Kill Bill: Vol 1',
+  'The Dark Knight Rises',
+  'The Silence of the Lambs',
+  'Shutter Island',
+  'The Godfather',
+  'The Lord of the Rings: The Return of the King',
+  'Pulp fiction',
+  'Schindlers List',
+  'Fight Club'
+]
+
+function newMovie() {
+  var randomNumber = Math.floor(Math.random() * (quotes.length));
+  document.getElementById('MovieDisplay').innerHTML = quotes[randomNumber];
+}
