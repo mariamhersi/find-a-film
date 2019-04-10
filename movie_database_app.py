476fb5a92ae847e0f175f17ced48e9063f661ee2
Riya-De <44146768+@users.noreply.github.com>
2019-04-10 19:53:35 +0100

    final changes

--- movie_database_app.py
+++ movie_database_app.py
@@ -8,23 +8,33 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("index 2.html")
+
+@app.route("/MoviesGenerator")
+def index_new():
+     return render_template("MoviesGenerator.html")
+
+@app.route("/display_movies", methods=["POST"])
+def display_movies():
     movies = []
-    if request.args.get("search") is not None:
-        search = request.args.get("search")
+    form_data = request.form
+    movie_name = form_data["search"]
+    if movie_name is not None:
     #else:
         # search = "star wars"
         #return render_template("error.html")
         # response = apirequest.get("http://www.omdbapi.com/?apikey=39907301&s=batman")
-        response = apirequest.get("http://www.omdbapi.com/?apikey=39907301&s=%s" % search)
+        response = apirequest.get("http://www.omdbapi.com/?apikey=39907301&s=%s" % movie_name)
         data = response.json()
         movies = data["Search"]
-        return render_template("index 2.html", movies=movies)
+        return render_template("index.html", movies=movies)
+    else:
+        return form_data["search"]
 #else:
         #return "hi"
 
 #@app.route("/<movie_name>")
 #def display_movie_data(movie_name):
-
 
 
 
